32e9faf9a80968575a0ae9a83a87593c98cd9eda
Zimnov1 <oleksandr.zimnov.shi.2023@lpnu.ua>
2024-10-04 10:37:53 +0300

    Add changes to Lab_2

--- Lab_2/Lab_2/Lab_2.py
+++ Lab_2/Lab_2/Lab_2.py
@@ -1,29 +1,31 @@
-
 class Client:
+    client_counter = 0  
+
     def __init__(self, client_id, name, surname, phone_number):
         self.client_id = client_id
         self.name = name
         self.surname = surname
-        self.phone_number = phone_number
+        self.__phone_number = phone_number  
+        Client.client_counter += 1  
 
-    def get_name(self):
-        return self.name
+    @staticmethod
+    def client_count():
+        return Client.client_counter  
 
-    def set_name(self, name):
-        self.name = name
+    @property
+    def phone_number(self):
+        return self.__phone_number
 
-    def get_phone_number(self):
-        return self.phone_number
-
-    def set_phone_number(self, phone_number):
-        self.phone_number = phone_number
+    @phone_number.setter
+    def phone_number(self, value):
+        self.__phone_number = value
 
     def display_client_info(self):
-        print(f"Client: {self.name} {self.surname}, Phone: {self.phone_number}")
+        print(f"Client: {self.name} {self.surname}, Phone: {self.__phone_number}")
 
     def update_phone_number(self, new_phone):
         self.phone_number = new_phone
-        print(f"Phone number updated to: {self.phone_number}")
+        print(f"Phone number updated to: {self.__phone_number}")
 
 
 class Payment:
@@ -55,6 +57,7 @@ class RepairApplication(Client, Device, Payment):
         print(f"Application ID: {self.application_id}, Status: {self.status}, Planned Price: ${self.planned_price}")
 
     def process_payment(self, amount):
+        super().process_payment(amount)  
         print(f"Payment for repair application {self.application_id}: ${amount}")
 
 
@@ -70,6 +73,31 @@ class Worker(Client):
 
     def assign_repair(self, application_id):
         print(f"Worker assigned to repair application {application_id}")
+
+
+class Technician(Payment):
+    def __init__(self, tech_id, name, surname):
+        self.tech_id = tech_id
+        self.name = name
+        self.surname = surname
+
+    def display_technician_info(self):
+        print(f"Technician: {self.name} {self.surname}, ID: {self.tech_id}")
+
+    def process_payment(self, amount):
+        super().process_payment(amount)
+        print(f"Technician payment processed for ${amount}")
+
+
+class RepairWorker(Worker, Technician):
+    def __init__(self, client_id, name, surname, phone, worker_id, position, tech_id):
+        Worker.__init__(self, client_id, name, surname, phone, worker_id, position)
+        Technician.__init__(self, tech_id, name, surname)
+
+    def display_repair_worker_info(self):
+        self.display_worker_info()
+        self.display_technician_info()
+
 
 client1 = Client(1, "John", "Doe", "123-456-789")
 client1.display_client_info()
@@ -88,3 +116,9 @@ app1.process_payment(250.75)
 app2.process_payment(150.50)
 
 worker1.assign_repair(1001)
+
+tech_worker = RepairWorker(3, "Sarah", "Connor", "555-987-654", 102, "Senior Technician", 201)
+tech_worker.display_repair_worker_info()
+tech_worker.process_payment(300.00)
+
+print(f"Total clients: {Client.client_count()}")  
